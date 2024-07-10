869b15b03ec60f12225d0504e5db3e284eafb9f5
Jhonatan Bergmann <54135650+jhonbergmann@users.noreply.github.com>
2024-07-10 19:42:17 -0300

    remove

--- app/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import './styles/global.css'
-
-import {Player} from './pages/Player'
-
-export function App() {
-  return <Player />
-}
--- app/src/components/Lesson/data.ts
+++
@@ -1,6 +0,0 @@
-export interface ILessonProps {
-  title: string
-  duration: string
-  isCurrent?: boolean
-  onPlay: () => void
-}
